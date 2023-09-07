26805ab2f84a7e852b424f6cba91c4ce52c4deeb
downlifted <38848227+downlifted@users.noreply.github.com>
2023-09-07 14:31:53 -0400

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+export const myEditionDropContractAddress: string = "0xbF7f561918f7a7A4E5270D69838FF56C3D5507E1";
 
 // Replace this with your token id
 export const tokenId: string = "0";
